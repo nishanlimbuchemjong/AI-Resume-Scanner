01a2dde00e192bef8263d74f2efc77b5e78e0ebd
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-02-12 12:53:54 +0545

    Configured the config.py file, and database initialized

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
+from config import Config
+from models import db
 
 app = Flask(__name__)
 
+# Load configuration from config.py file
+app.config.from_object(Config)
 
+# Initialize database
+db.init_app(app)
 
 @app.route('/')
 def index():
