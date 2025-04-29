ab4f6e979724d76c5b8ddd171a49d44e2f6d3dba
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-04-29 18:08:31 +0545

    all database related credentials like host, user, password, dbname, port changed or updated

--- database.py
+++ database.py
@@ -1,12 +1,18 @@
 from sqlalchemy import create_engine
 import psycopg2
+from dotenv import load_dotenv
+import os
 
-host = "localhost"
-user = "postgres"
-password = "testing"
-dbname = "AI_Resume_Scanner"
-port = 5432
+# Load environment variables from .env file
+load_dotenv()
 
+host = os.getenv("DB_HOST")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+dbname = os.getenv("DB_NAME")
+port = int(os.getenv("DB_PORT"))
+
+# Construct the database URL using environment variables
 DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{dbname}"
 engine = create_engine(DATABASE_URL)
 
